e55ac94e69800904503a4709f252dbdf2d0a1575
logicador <logicador@naver.com>
2021-01-10 18:15:29 +0900

    upload test

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/login', function(req, res) {
-    res.render('admin/index', { menu: 'login' })
-})
+// router.get('/login', function(req, res) {
+//     res.render('admin/index', { menu: 'login' })
+// });
 
 
-router.get('/', function(req, res) {
-    if (!req.session.is_admin) {
-        res.redirect('/admin/login');
-        return;
-    }
+// router.get('/', function(req, res) {
+//     if (!req.session.is_admin) {
+//         res.redirect('/admin/login');
+//         return;
+//     }
 
-    res.render('admin/index', { menu: 'main' });
-});
+//     res.render('admin/index', { menu: 'main' });
+// });
 
 
 module.exports = router;
--- routes/webapi.js
+++ routes/webapi.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
 // const exec = require('child_process').exec;
-// require('dotenv').config();
 var request = require('request');
 const pool = require('../lib/database');
 
@@ -204,6 +203,109 @@ router.get('/get/places', (req, res) => {
                 res.json({ status: 'OK', result: { placeList: placeList } });
                 return;
             });
+        });
+    });
+
+});
+
+
+router.post('/posting', (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.uploadDir = 'upload/tmp';
+    form.multiples = true;
+    form.keepExtensions = true;
+
+    form.parse(req, (error, body, files) => {
+        if (error) {
+            res.json({ status: 'ERR_UPLOAD' });
+            return;
+        }
+
+        let uId = body.uId;
+        let pId = body.pId;
+
+        console.log(uId, pId);
+
+        res.json({ status: 'OK' });
+    });
+});
+
+
+//이미지 저장 
+router.post('/upload/image', async (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.uploadDir = 'upload/tmp';
+    form.multiples = true;
+    form.keepExtensions = true;
+
+    form.parse(req, function(error, body, files) {
+        if (error) {
+            res.json({ status: 'ERR_UPLOAD' });
+            return;
+        }
+
+        let dataType = body.dataType;
+        let type = body.type; // THUMB, IMAGE, IMAGE_DETAIL
+        let targetId = body.targetId; // 데이터 아이디
+        
+        let imageName = f.generateRandomId();
+        let imageFilePath = `public/images/${dataType}/${imageName}_original.jpg`;
+
+        let reImageFilePath = `public/images/${dataType}/${imageName}.jpg`;
+        let reImagePath = `/images/${dataType}/${imageName}.jpg`;
+
+        fs.rename(files.image.path, imageFilePath, function() {
+
+            let stats = fs.statSync(imageFilePath);
+            let originFileSize = stats.size;
+            let originWidth = imageSize(imageFilePath).width;
+
+            let rw = 0;
+
+            fs.copyFile(imageFilePath, reImageFilePath, async () => {
+
+                if (originFileSize < 200000) {
+        
+                } else {
+                    let reSize = originFileSize;
+                    let per = 0;
+                    while (reSize > 200000) {
+                        per += 5;
+                        rw = parseInt(originWidth * ((100 - per) / 100));
+                        await sharp(imageFilePath)
+                            .resize({width: rw})
+                            .toFile(reImageFilePath);
+                        reSize = fs.statSync(reImageFilePath).size;
+                    }
+                }
+    
+                if (type === 'THUMB') {
+                    // UPDATE data thumbnail
+                    let query = '';
+                    let params = [reImagePath, targetId];
+    
+                    if (dataType === 'food') {
+                        query = 'UPDATE t_foods SET f_thumbnail = ? WHERE f_id = ? ';
+                    } else if (dataType === 'product') {
+                        query = 'UPDATE t_products SET p_thumbnail = ? WHERE p_id = ?';                    
+                    } else {
+                        res.json({status: 'ERR_WRONG_DATA_TYPE'});
+                        return;
+                    }
+                    let [result, fields] = await pool.query(query, params);
+    
+                } 
+                 else {
+                    // INSERT images
+                    let query = "INSERT INTO t_images (i_type, i_path, i_target_id, i_data_type) VALUES (?, ?, ?, ?)";
+                    let params = [type, reImagePath, targetId, dataType];
+                    let [result, fields] = await pool.query(query, params);
+                }
+                res.json({ status: 'OK', reImagePath: reImagePath });
+            });
+
         });
     });
 
